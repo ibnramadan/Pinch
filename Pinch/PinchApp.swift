d7cd6fd32abe6feb6832f7dfc42a3131e7dc2919
Mohamed Ramadan <mohamed.moslem50@gmail.com>
2023-12-04 10:35:18 +0200

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by mohamed ramadan on 04/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
